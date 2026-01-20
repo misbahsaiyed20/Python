30c332b059d41fa5c4ef05e7b90c830abf2393f1
misbahsaiyed20 <misbasaiyed20@gmail.com>
2026-01-20 07:26:24 +0530

    Delete task_using_marshmallow directory

--- task_using_marshmallow/schemas.py
+++
@@ -1,6 +0,0 @@
-from marshmallow import Schema, fields
-
-class UserSchema(Schema):
-    name = fields.Str(required=True)
-    email = fields.Email(required=True)
-    age = fields.Int(required=True)
