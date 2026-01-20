63a5ea4da4a37e5f3cf51709c8193dba9a84a0c1
misbahsaiyed20 <misbasaiyed20@gmail.com>
2026-01-20 07:13:47 +0530

    Add files via upload
    input validation with marshmallow

---
+++ task_using_marshmallow/app.py
@@ -0,0 +1,17 @@
+from schemas import UserSchema
+
+schema = UserSchema()
+
+user_data = {
+    "name": "Misba",
+    "email": "misbaemail",
+    "age": "twenty"
+}
+
+# checking user input using marshmallow
+errors = schema.validate(user_data)
+
+if errors:
+    print("Input validation failed:", errors)
+else:
+    print("All inputs are valid")
---
+++ task_using_marshmallow/schemas.py
@@ -0,0 +1,6 @@
+from marshmallow import Schema, fields
+
+class UserSchema(Schema):
+    name = fields.Str(required=True)
+    email = fields.Email(required=True)
+    age = fields.Int(required=True)
